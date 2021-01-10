034fb4d62045c5419aa6008a382fffa8bdc53564
Mudit Gupta <muditg317@gmail.com>
2021-01-10 18:12:32 -0500

    hopefully major color improvment

--- frontend/src/components/mobile-tasks/tracer/tracer.jsx
+++ frontend/src/components/mobile-tasks/tracer/tracer.jsx
@@ -1,8 +1,8 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import Sketch from 'polyfills/react-p5';
 
-import { randInRange } from 'utils';
-// TODO: copy navigaation: chart course
+import { useTaskFinish } from 'hooks';// TODO: add useP5event
+import { map, randInRange, P5tools } from 'utils';
 
 const BOARD_WIDTH = 250;
 const BOARD_HEIGHT = BOARD_WIDTH / 2;
@@ -17,10 +17,17 @@ const NUM_POINTS = 4;
 const MAX_TRACING_ERROR = 25; // number of pixels you can deviate from path without losing hold
 const INCR_X = BOARD_WIDTH / (NUM_POINTS + 1);
 
-const BACKGROUND_COLOR = [88, 130, 209];
+const BACKGROUND_COLOR = [3, 25, 69];
+const STAR_POSITIONS = (() => {
+  const positions = [];
+  for (let i = 0; i < 50; i++) {
+    positions.push({x: randInRange(BOARD_WIDTH), y: randInRange(BOARD_HEIGHT)});
+  }
+  return positions;
+})();
 const TRACER_COLOR = [153, 180, 232];//176, 196, 235|131, 164, 230
-const POINT_COLOR = [190, 196, 207];
-const REACHED_POINT_COLOR = [232, 238, 250];
+const POINT_COLOR = [255, 247, 135];
+const REACHED_POINT_COLOR = [255, 242, 66];
 const PATH_COLOR = [209, 213, 222];
 
 
@@ -40,6 +47,7 @@ const generateRandomPath = () => {
 
 const Tracer = (props) => {
   const { finish, onExit } = props;
+  const [ finished, finishTask ] = useTaskFinish(finish, onExit, 750);
   const completeTask = useCallback(() => {
     // console.log('complete');
     finish();
@@ -47,7 +55,6 @@ const Tracer = (props) => {
   }, [finish, onExit]);
 
   const [ pathPoints, ] = useState(generateRandomPath);
-  const [ finished, setFinished ] = useState(false);
   const [ progress, setProgress ] = useState(1); // which point are you on
   const [ holding, setHolding ] = useState(false); // is the mouse controlling the position of the tracer
   // const currentCheckpoint = useMemo(() => pathPoints[progress], [pathPoints, progress]);
@@ -78,38 +85,55 @@ const Tracer = (props) => {
     finished ? _setX(newX) : _setX(Math.min(Math.max(newX, currentLine.p1.x), BOARD_WIDTH - INCR_X));
   }, [finished, currentLine]);
 
-  const finishedTimeoutRef = useRef();
-
   const setup = useCallback((p5) => {
     p5.createCanvas(BOARD_WIDTH, BOARD_HEIGHT);
     p5.noStroke();
     p5.background(...BACKGROUND_COLOR);
   }, []);
 
+  const drawBackground = useCallback((p5) => {
+    p5.background(...BACKGROUND_COLOR);
+    p5.push();
+    p5.fill(...POINT_COLOR);
+    p5.noStroke();
+    STAR_POSITIONS.forEach(star => {
+      // console.log(star);
+      P5tools.star(p5, star.x, star.y, 2,3, Math.floor(map(Math.sin(p5.millis() / 50), -1,1, 5,7)));
+    });
+    p5.pop();
+  }, []);
+
+  const drawPath = useCallback((p5, points, prog) => {
+    points.forEach((point, index) => {
+      if (index < points.length - 1) {
+        const nextPoint = points[index+1];
+        const color = [...PATH_COLOR, index < prog-1 ? 200 : (Math.sin(p5.millis() / 200) + 1) / 2 * 100+50];
+        p5.stroke(...color);
+        p5.strokeWeight(DOTTED_WIDTH);
+        if (index >= prog-1) {
+          p5.drawingContext.setLineDash([5, 10]);
+          p5.line(point.x, point.y, nextPoint.x, nextPoint.y);
+          p5.drawingContext.setLineDash([]);
+          p5.stroke(...BACKGROUND_COLOR);
+          p5.strokeWeight(DOTTED_WIDTH-2);
+          p5.line(point.x, point.y, nextPoint.x, nextPoint.y);
+          p5.stroke(...color);
+          p5.strokeWeight(PATH_WIDTH);
+        }
+        p5.line(point.x, point.y, nextPoint.x, nextPoint.y);
+        p5.noStroke();
+      }
+      p5.fill(...(index < prog ? REACHED_POINT_COLOR : POINT_COLOR));
+      // p5.circle(point.x,point.y, POINT_SIZE);
+      P5tools.star(p5, point.x,point.y, POINT_SIZE/3,POINT_SIZE/2, 5);
+    });
+  }, []);
+
   const draw = useCallback((p5) => {
     if (finished) {
       setProgress(prev => prev + 1.5);
-      p5.background(...BACKGROUND_COLOR);
-      pathPoints.forEach((point, index) => {
-        if (index < pathPoints.length - 1) {
-          const nextPoint = pathPoints[index+1];
-          const color = [...PATH_COLOR, 200];
-          p5.stroke(...color);
-          p5.strokeWeight(DOTTED_WIDTH);
-          // p5.drawingContext.setLineDash([10, 10]);
-          // p5.line(point.x, point.y, nextPoint.x, nextPoint.y);
-          // p5.drawingContext.setLineDash([]);
-          // p5.stroke(...BACKGROUND_COLOR);
-          // p5.strokeWeight(DOTTED_WIDTH-2);
-          // p5.line(point.x, point.y, nextPoint.x, nextPoint.y);
-          // p5.stroke(...color);
-          // p5.strokeWeight(PATH_WIDTH);
-          p5.line(point.x, point.y, nextPoint.x, nextPoint.y);
-          p5.noStroke();
-        }
-        p5.fill(...(index < progress ? REACHED_POINT_COLOR : POINT_COLOR));
-        p5.circle(point.x,point.y, POINT_SIZE);
-      });
+      drawBackground(p5);
+      drawPath(p5, pathPoints, NUM_POINTS);
       p5.fill(...TRACER_COLOR);
       p5.push();
       p5.translate(BOARD_WIDTH/2,BOARD_HEIGHT/2);
@@ -124,10 +148,7 @@ const Tracer = (props) => {
         if (progress < NUM_POINTS-1) {
           setProgress(prev => prev + 1);
         } else {
-          setFinished(true);
-          finishedTimeoutRef.current = setTimeout(() => {
-            completeTask();
-          }, 750);
+          finishTask();
         }
       }
     } else {
@@ -136,29 +157,8 @@ const Tracer = (props) => {
       xOff && setX(x + delta);
       // setY(y + delta*currentLine.slope);
     }
-    p5.background(...BACKGROUND_COLOR);
-    pathPoints.forEach((point, index) => {
-      if (index < pathPoints.length - 1) {
-        const nextPoint = pathPoints[index+1];
-        const color = [...PATH_COLOR, index < progress-1 ? 200 : (Math.sin(p5.millis() / 200) + 1) / 2 * 100+50];
-        p5.stroke(...color);
-        p5.strokeWeight(DOTTED_WIDTH);
-        if (index >= progress-1) {
-          p5.drawingContext.setLineDash([10, 10]);
-          p5.line(point.x, point.y, nextPoint.x, nextPoint.y);
-          p5.drawingContext.setLineDash([]);
-          p5.stroke(...BACKGROUND_COLOR);
-          p5.strokeWeight(DOTTED_WIDTH-2);
-          p5.line(point.x, point.y, nextPoint.x, nextPoint.y);
-          p5.stroke(...color);
-          p5.strokeWeight(PATH_WIDTH);
-        }
-        p5.line(point.x, point.y, nextPoint.x, nextPoint.y);
-        p5.noStroke();
-      }
-      p5.fill(...(index < progress ? REACHED_POINT_COLOR : POINT_COLOR));
-      p5.circle(point.x,point.y, POINT_SIZE);
-    });
+    drawBackground(p5);
+    drawPath(p5, pathPoints, progress);
     p5.push();
     p5.fill(...TRACER_COLOR);
     // p5.strokeWeight(2);
@@ -167,7 +167,7 @@ const Tracer = (props) => {
     p5.rotate(currentLine.angle-Math.PI/2);
     p5.triangle(-TRACER_SIZE/2, -TRACER_SIZE/2, TRACER_SIZE/2, -TRACER_SIZE/2, 0, TRACER_SIZE);
     p5.pop();
-  }, [finished,completeTask, pathPoints,progress, holding,x,currentLine,setX]);
+  }, [finished,finishTask, drawBackground,drawPath, pathPoints,progress, holding,x,currentLine,setX]);
 
   const mousePressed = useCallback((p5, event) => {
     if (!(p5.mouseX <= BOARD_WIDTH+INTERACTION_MARGIN && p5.mouseX >= -INTERACTION_MARGIN && p5.mouseY <= BOARD_HEIGHT+INTERACTION_MARGIN && p5.mouseY >= -INTERACTION_MARGIN)) {
@@ -214,21 +214,6 @@ const Tracer = (props) => {
     return false;
   }, []);
   const touchEnded = mouseReleased;
-
-  // useEffect(() => {
-  //   spawnEnemyIntervalRef.current = setInterval(() => {
-  //     updateEnemies({spawnEnemy: { playerSize }});
-  //   }, 1000 / ENEMY_SPAWN_RATE);
-  //   return () => {
-  //     clearInterval(spawnEnemyIntervalRef.current);
-  //   }
-  // }, [playerSize]);
-
-  useEffect(() => {
-    return () => {
-      clearTimeout(finishedTimeoutRef.current);
-    };
-  }, []);
 
   return (
     <>
