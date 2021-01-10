fa6e8b90eb76ee7fa062ea188ca7bd900dbe95a6
Mudit Gupta <muditg317@gmail.com>
2021-01-10 18:12:05 -0500

    add P5tools in utils

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,6 +1,7 @@
 import * as Colors from './Colors';
 import * as Utils from './Utils';
 import * as Constants from './Constants';
+import * as P5tools from './P5tools';
 
 export * from './Colors';
 export { Colors };
@@ -8,3 +9,5 @@ export * from './Utils';
 export { Utils };
 export * from './Constants';
 export { Constants };
+export * from './P5tools';
+export { P5tools };
